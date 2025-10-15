4046205fb83e3f546753c19c41206379cfeb770f
Filip <f.jaros05@seznam.cz>
2025-10-15 20:32:31 +0200

    adapt JS code and make it applicable on multiple pages

--- vesnice/render_images.js
+++ vesnice/render_images.js
@@ -2,29 +2,32 @@ let gallery = null
 let small_images = null
 let zoom_image = null
 
-window.onload = async () => {
+// na body se da onload a zavola se tahle funkce. Do path se ulozi cesta k obrazkum
+// tahle cesta se preda funkci fill_gallery.
+// poznamka (pokud se nacita z aktualniho adresare jako argument se musi nastvit tecka)
+async function check(path){
 
     // nacti galerii a pokud existuje tak ji napln
     gallery = document.getElementById('gallery')
     // fill_gallery je asynchrini funkce takze se pocka az se ukonci
-    if(gallery) await fill_galery()
+    if(gallery) await fill_galery(path)
 
     // nacti DOM obrazku v galerii a div ve kterem se zvetsi
     small_images = document.querySelectorAll('.small_image')
     zoom_image = document.getElementById('zoom_image')
-    console.log(small_images)
     if(small_images && zoom_image) find_active_image()
+    
 }
 
 // naplni galerii
-async function fill_galery(){
+async function fill_galery(path){
 
     let count = 1
     let exist = true
 
     while (exist) {
         const image = new Image()
-        image.src = `${count}.jpg`
+        image.src = `${path}/${count}.jpg`
 
         exist = await new Promise(resolve => {
             image.onload = () => resolve(true)
@@ -45,9 +48,9 @@ function find_active_image(){
     let images_array = Array.from(small_images)
 
     images_array.forEach((event) => {
-        console.log(event)
         event.addEventListener('click', () => {
-            console.log('click')        // jeste se musi dodelat
+            zoom_image.classList.add('zoom_active')
+            zoom_image.appendChild(event)
         })
     })
 }
