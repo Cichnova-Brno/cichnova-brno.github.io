786cf49b9f7680d1f9e41efc726e05fc718f1c92
Leminix <f.jaros05@seznam.cz>
2025-10-21 21:08:06 +0200

    create graph

---
+++ vesnice/graph.js
@@ -0,0 +1,41 @@
+function print_graph(xvalues, yvalues, max, jump){
+
+  window.addEventListener('DOMContentLoaded', () => {
+    const ctx = document.getElementById('data').getContext('2d')
+    const barColors = '#d9d9d9'
+    new Chart(ctx, {
+      type: "bar",
+      data: {
+        labels: xvalues,
+        datasets: [{
+          backgroundColor: barColors,
+          data: yvalues
+        }]
+      },
+
+      options: {
+        legend: {
+          display: false
+        },
+          
+        elements: {
+          rectangle: {
+            borderRadius: 10
+          }  
+        },
+
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true,
+              min: 0,
+              max: max,
+              stepSize: jump
+            }
+          }]
+        }
+      }
+    })
+  })
+}
+print_graph()
