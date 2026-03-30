28e543ccf59b81e4093b4dff80df16773fddca03
Filip <f.jaros05@seznam.cz>
2026-03-30 19:20:05 +0200

    phase cursor remove redundant variable

--- phases_crsor.js
+++ phases_crsor.js
@@ -1,6 +1,5 @@
 
 let all_text_elements = document.querySelectorAll('text')
     all_text_elements.forEach(e => {
-        let child = e.children
         if(e.textContent.includes('Etapa')) e.style.cursor = 'default'
 })
